e4c101c01ad78e432d0223486736e6cc7fe4a450
th-schmidt <thomas.schmidt@uni-mannheim.de>
2025-07-04 14:55:04 +0200

    Remove OpenAI vectorstore flag from config

--- code/config.py
+++ code/config.py
@@ -9,9 +9,6 @@ CHUNK_OVERLAP = 0
 RSS_URL = "https://blog.bib.uni-mannheim.de/Aktuelles/?feed=rss2&cat=4"
 DB_PATH = "../data/feedback.db"
 
-# OpenAI Vectorstore Option
-USE_OPENAI_VECTORSTORE = False
-
 # Session Memory Configuration
 SESSION_MEMORY_CONFIG = {
     "max_turns": 10,           # Maximum conversation turns to remember
